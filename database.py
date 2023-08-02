f8c031199d894805be5f421b4586abc5f9cfdb86
Neel Patel <29038590+N-NeelPatel@users.noreply.github.com>
2023-08-02 16:42:01 +0530

    added comment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
+# sql alchemy db url
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/olist_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
